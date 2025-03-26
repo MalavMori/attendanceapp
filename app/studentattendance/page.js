71039a8e082bda5ffb686f0788934080a84ebd18
MalavMori <malavmori24@gmail.com>
2025-03-26 17:02:04 +0530

    gps timeout update to 20000

--- app/studentattendance/page.js
+++ app/studentattendance/page.js
@@ -113,7 +113,7 @@ const StudentaAttendance = () => {
             {
               enableHighAccuracy: true,
               maximumAge: 0,
-              timeout: 10000,
+              timeout: 20000,
             }
           );
         } else {
