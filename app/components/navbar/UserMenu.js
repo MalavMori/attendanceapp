c8f8a1092f127386593f520473abeb6374e1ec61
MalavMori <malavmori24@gmail.com>
2025-04-07 16:33:10 +0530

    PWA Add

--- app/components/navbar/UserMenu.js
+++ app/components/navbar/UserMenu.js
@@ -1,17 +1,21 @@
 import React from "react";
 import { IconLogout } from "@tabler/icons-react";
-import {  Menu } from "@mantine/core";
+import { Menu } from "@mantine/core";
+import { signOut } from "next-auth/react";
 
-
-const UserMenu = ({target}) => {
+const UserMenu = ({ target }) => {
   return (
     <>
       <Menu shadow="md" width={200}>
-        <Menu.Target>
-          {target}
-        </Menu.Target>
-        <Menu.Dropdown >
-          <Menu.Item color="red" leftSection={<IconLogout size={14} />}>
+        <Menu.Target>{target}</Menu.Target>
+        <Menu.Dropdown>
+          <Menu.Item
+            color="red"
+            onClick={() => {
+              signOut();
+            }}
+            leftSection={<IconLogout size={14} />}
+          >
             Logout
           </Menu.Item>
         </Menu.Dropdown>
--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,23 @@
 /** @type {import('next').NextConfig} */
+
+import withPWAInit from "@ducanh2912/next-pwa";
+
+const withPWA = withPWAInit({
+  dest: "public",
+  cacheOnFrontEndNav:true,
+  aggressiveFrontEndNavCaching:true,
+  reloadOnOnline:true,
+  disable:false,
+  workboxOptions:{
+    disableDevLogs:true
+  },
+  
+});
+
 const nextConfig = {
-    experimental: {
-        optimizePackageImports: ['@mantine/core', '@mantine/hooks'],
-      },
+  experimental: {
+    optimizePackageImports: ["@mantine/core", "@mantine/hooks"],
+  },
 };
 
-export default nextConfig;
+export default withPWA(nextConfig);
